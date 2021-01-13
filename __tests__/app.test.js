1c92f7090075b7e56629d0d2b83830e9a4f9b33d
Sydney James <sydneyjames7@gmail.com>
2021-01-13 09:34:18 -0800

    intitial test commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,10 @@ const app = require('../lib/app');
 
 describe('realo-app-backend routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('', () => {
+    expect(true).toEqual(true);
   });
 });
