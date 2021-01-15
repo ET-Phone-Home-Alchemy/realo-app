725345d1fcd5a7c1038fec5f984fdead03597e7b
Your NamejeffIrvine <jirvine1212@gmail.com>
2021-01-15 11:15:07 -0800

    failing test on user login

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 
 const agent = request.agent(app);
@@ -32,6 +33,30 @@ describe('realo-app-backend routes', () => {
       name: 'Jon Arbuckle',
       phoneNumber: '1235671234',
       carrier: 'att'
+    });
+  });
+
+
+  it('lets a user login', async() => {
+
+    const user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password',
+      name: 'Joan Arbuckle',
+      phoneNumber: '1235679876',
+      carrier: 'att' 
+    }); 
+
+    const res = await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test1@test.com',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      userId: user.id,
+      email: 'test@test.com'
     });
   });
 
