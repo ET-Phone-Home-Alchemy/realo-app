df9f23d3ef521496148c025746e56e76358b9c00
kevinfiero <kjf46@cornell.edu>
2021-01-14 17:46:55 -0800

    scrape improvements

--- sql/scrape.sql
+++ sql/scrape.sql
@@ -4,7 +4,6 @@ CREATE TABLE listings(
 	id INTEGER NOT NULL PRIMARY KEY,
 	source TEXT NOT NULL,
 	address TEXT NOT NULL,
-	zip_code INTEGER NOT NULL,
 	link TEXT NOT NULL,
 	price INTEGER,
 	square_feet INTEGER,
