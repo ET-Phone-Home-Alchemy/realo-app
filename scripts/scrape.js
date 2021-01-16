ccbf09bb274dedba14f331ced9fa4282da89209e
Sydney James <69941872+scjam@users.noreply.github.com>
2021-01-15 16:17:32 -0800

    Feature/card 6 g scrape all (#12)
    * Initial Commits (#1)
    
    * initial commit
    
    * intitial test commit
    
    * making sure heroku deploys
    
    * sandbox cheerio
    
    * scraped correct information from zillow for front page
    
    * testing merge conflicts
    
    * added project requirements markdown for assignment
    
    * attempting to scrape all, link might be broken
    
    * scrape all works apparently

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,78 +1,88 @@
-const request = require('request');
+const Throttle = require('superagent-throttle');
+const request = require('superagent');
 const cheerio = require('cheerio');
-const fs = require('fs');
-const pool = require('../lib/utils/pool.js');
+// const fs = require('fs');
+// const pool = require('../lib/utils/pool.js');
 const Listing = require('../lib/models/Listing.js');
 
-const searchLinkFront = 'https://www.zillow.com/portland-or/1-_beds/2_p/?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A';
-const searchLinkBack = '%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.77514839172363%2C%22east%22%3A-122.16839790344238%2C%22south%22%3A45.211129097221004%2C%22north%22%3A45.98175482781839%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A13373%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22beds%22%3A%7B%22min%22%3A1%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D';
+const throttle = new Throttle({ rate: 1, ratePer: 5000, concurrent: 1 });
 
-pool.query(fs.readFileSync('./sql/scrape.sql', 'utf-8'));
+//pool.query(fs.readFileSync('./sql/scrape.sql', 'utf-8'));
 let page;
-for(page = 1; page <= 20; page++){
-  console.log(page);
-  const searchLink = searchLinkFront + page + searchLinkBack;
+let bed; 
+for(page = 1; page <= 10; page++){
+  for(bed = 0; bed <= 10; bed++){
+    const searchLink = `https://www.zillow.com/portland-or/?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A${page}%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.11454775976563%2C%22east%22%3A-122.27684024023438%2C%22south%22%3A45.004136336507614%2C%22north%22%3A46.10132794557867%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A13373%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22beds%22%3A%7B%22min%22%3A${bed}%2C%22max%22%3A${bed}%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%7D%2C%22isListVisible%22%3Atrue%7D
+  `;
 
-  request(searchLink, (error, response, html) => {
-    if(!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
+    request(searchLink)
+      .set('origin', 'true')
+      .set('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36')
+      .use(throttle.plugin())
+      .then(response => {
+        if(response.statusCode == 200) {
+          const html = response.text;
+          const $ = cheerio.load(html);
+          const timeStamp = new Date();
 
-      const timeStamp = new Date();
+          $('.list-card').each((i, element) => {
+            const price = $(element)
+              .find('.list-card-price')
+              .text()
+              .replace(/\D/g, '') || 0;
 
-      $('.list-card').each((i, element) => {
-        const price = $(element)
-          .find('.list-card-price')
-          .text()
-          .replace(/\D/g, '') || null;
-
-        const bed = $(element)
-          .find('.list-card-details li:nth-child(1)')
-          .text()
-          .replace(/\D/g, '') || null;
+            const bed = $(element)
+              .find('.list-card-details li:nth-child(1)')
+              .text()
+              .replace(/\D/g, '') || 0;
       
-        const bath = $(element)
-          .find('.list-card-details li:nth-child(2)')
-          .text()
-          .replace(/\D/g, '') || null;
+            const bath = $(element)
+              .find('.list-card-details li:nth-child(2)')
+              .text()
+              .replace(/\D/g, '') || 0;
 
-        const squareFeet = $(element)
-          .find('.list-card-details li:nth-child(3)')
-          .text()
-          .replace(/\D/g, '') || null;
+            const squareFeet = $(element)
+              .find('.list-card-details li:nth-child(3)')
+              .text()
+              .replace(/\D/g, '') || 0;
 
-        const address = $(element)
-          .find('.list-card- addr')
-          .text();
+            const address = $(element)
+              .find('.list-card-addr')
+              .text();
 
-        const link = $(element)
-          .find('a')
-          .attr('href');
+            const link = $(element)
+              .find('a')
+              .attr('href');
 
-        const id = $(element)
-          .attr('id')
-          .replace(/\D/g, '');
+            const id = $(element)
+              .attr('id')
+              .replace(/\D/g, '');
 
-        const listing = {
-          id,
-          source: 'Zillow',
-          address,
-          link,
-          price,
-          squareFeet,
-          bed,
-          bath,
-          scrapeTimestamp: timeStamp
-        };
+            const listing = {
+              id,
+              source: 'Zillow',
+              address,
+              link,
+              price,
+              squareFeet,
+              bed,
+              bath,
+              scrapeTimestamp: timeStamp
+            };
 
-        Listing.insert(listing);
-      });
+            console.log(listing);
+            Listing.insert(listing);
+          });
 
-    }
+        }
+      }
+
+      );
+
   }
-
-  );
 }
 
 //pool.end();
 console.log('Scraping Done...');
+
 
