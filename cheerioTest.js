901faf6d8b807fb6ee7ceb287efdc3adb97cfeeb
Sydney James <sydneyjames7@gmail.com>
2021-01-15 16:12:38 -0800

    scrape all works apparently

--- cheerioTest.js
+++ cheerioTest.js
@@ -3,47 +3,37 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('post.csv');
 
-request('https://www.zillow.com/portland-or/', (error, response, html) => {
+request('https://www.redfin.com/city/30772/OR/Portland/filter/sort=lo-days', (error, response, html) => {
   if(!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
 
-    $('.list-card').each((i, element) => {
+    $('.v2-interactive').each((i, element) => {
       const price = $(element)
-        .find('.list-card-price')
+        .find('.homecardV2Price')
         .text()
         .replace(/\D/g, '');
 
       const bed = $(element)
-        .find('.list-card-details li:nth-child(1)')
+        .find('.HomeStatsV2 li:nth-child(1)')
         .text()
         .replace(/\D/g, '');
 
       const bath = $(element)
-        .find('.list-card-details li:nth-child(2)')
+        .find('.HomeStatsV2 li:nth-child(2)')
         .text()
         .replace(/\D/g, '');
 
       const sqft = $(element)
-        .find('.list-card-details li:nth-child(3)')
+        .find('.HomeStatsV2 li:nth-child(3)')
         .text()
         .replace(/\D/g, '');
 
       const address = $(element)
-        .find('.list-card-addr')
+        .find('.link-and-anchor')
         .text();
 
-      const zipcode = address.substring(address.length - 5, address.length);
-
-      const link = $(element)
-        .find('a')
-        .attr('href');
-
-      const zpid = $(element)
-        .attr('id')
-        .replace(/\D/g, '');
-
       // Write Row To CSV
-      writeStream.write(`${price}, ${bed}, ${bath}, ${sqft}, ${address}, ${zipcode}, ${link}, ${zpid} \n`);
+      writeStream.write(`${price}, ${bed}, ${bath}, ${sqft}, ${address} \n`);
     });
 
     console.log('Scraping Done...');
