019e5c34fd5e07f72cf190f307eae848c297ca27
kevinfiero <kjf46@cornell.edu>
2021-01-18 15:37:03 -0800

    create SQL for filters table and INSERT filters

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -9,6 +9,7 @@ const agent = request.agent(app);
 describe('realo-app-backend routes', () => {
   beforeEach(async() => {
     await pool.query(fs.readFileSync('./sql/filter.sql', 'utf-8'));
+    await pool.query(fs.readFileSync('./sql/auth.sql', 'utf-8'));
 
     await UserService.create({
       email: 'test@test.com',
@@ -50,17 +51,17 @@ describe('realo-app-backend routes', () => {
       });
 
     expect(res.body).toEqual({
-      filterId: 1,
-      userId: 1,
+      filterId: '1',
+      userId: '1',
       filterName: 'My first filter!',
-      squareFeetMin: 0,
-      squareFeetMax: 10000,
-      bedMin: 0,
-      bedMax: 7,
-      bathMin: 0,
-      bathMax: 8,
-      priceMin: 0,
-      priceMax: 1000000
+      squareFeetMin: '0',
+      squareFeetMax: '10000',
+      bedMin: '0',
+      bedMax: '7',
+      bathMin: '0',
+      bathMax: '8',
+      priceMin: '0',
+      priceMax: '1000000'
     });
   });
 
