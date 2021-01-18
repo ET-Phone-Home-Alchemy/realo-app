c1e9dc8531c4ac5953adf1d4eb17c9be53ea4aef
Kevin Fiero <71036972+kevinfiero@users.noreply.github.com>
2021-01-18 13:54:03 -0800

    Bug/timestamp (#16)
    * fixed link
    
    * adjust timestamp outside loop

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -10,6 +10,8 @@ const throttle = new Throttle({ rate: 1, ratePer: 5000, concurrent: 1 });
 //pool.query(fs.readFileSync('./sql/scrape.sql', 'utf-8'));
 let page;
 let bed; 
+const timeStamp = new Date();
+
 for(page = 1; page <= 10; page++){
   for(bed = 0; bed <= 10; bed++){
     //const searchLink = `https://www.zillow.com/portland-or/?searchQueryState=%7B%22pagination%22%3A%7B%22currentPage%22%3A${page}%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.11454775976563%2C%22east%22%3A-122.27684024023438%2C%22south%22%3A45.004136336507614%2C%22north%22%3A46.10132794557867%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A13373%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22beds%22%3A%7B%22min%22%3A${bed}%2C%22max%22%3A${bed}%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%7D%2C%22isListVisible%22%3Atrue%7D`;
@@ -22,7 +24,6 @@ for(page = 1; page <= 10; page++){
         if(response.statusCode == 200) {
           const html = response.text;
           const $ = cheerio.load(html);
-          const timeStamp = new Date();
 
           $('.list-card').each((i, element) => {
             const price = $(element)
