fa09da3dbe9201745c0fb67f3522beb2852312c2
jeffIrvine <68131101+jeffIrvine@users.noreply.github.com>
2021-01-21 10:34:00 -0800

    Feature/card 8e landing page design (#58)
    * test for user signup /POST, will need to refactor with cookie later
    
    * created new file with user sql table
    
    * created static insert in user model
    
    * added route to app.js, added middleware to auth.js, fixed insert user test
    
    * added route to app.js, added middleware to auth.js, fixed insert user test
    
    * failing test on user login
    
    * working login test
    
    * working test on login
    
    * wrote verify, test failing as expected
    
    * removing dup code
    
    * deleted dupe code in test file
    
    * git mergining
    
    * cleaning up file structure auth to user
    
    * built home page and signup with drop down of carrrriers
    
    * fixed carrier map on pug side, signing up sends text automagically
    
    * github ci testing
    
    * fixed package-lockjson for github ci
    
    * fixed package-lockjson for github ci
    
    * fixed package-lockjson for github ci
    
    * merge resolution
    
    * redirect on signup
    
    * redirect on signup
    
    * added pug layout page login and logout button
    
    * fixed login routing and logout delete cookie and redirect
    
    * added pug layout page login and logout button
    
    * fixed logout button with clear cookie and redirect
    
    * added new home page and nav buttons for login and signup pages, styled and hover animations added
    
    Co-authored-by: ErikGraciosa <erikgraciosa@gmail.com>

--- views/js/buttonLogic.js
+++ views/js/buttonLogic.js
@@ -1,11 +1,5 @@
 const logoutButton = document.getElementById('logoutButton');
-const logInButton = document.getElementById('logInButton');
 
 logoutButton.addEventListener('click', () => {
   window.location = '/logout';
-});
-
-
-logInButton.addEventListener('click', () => {
-  window.location = '/login';
 });
