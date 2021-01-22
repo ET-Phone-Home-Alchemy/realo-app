cf1b8d0d4e551eb690beaebb1c162ff08c94e40f
Sydney James <sydneyjames7@gmail.com>
2021-01-22 09:28:52 -0800

    fixed delete button

--- views/js/filters.js
+++ views/js/filters.js
@@ -1,8 +1,6 @@
 const deleteFilter = document.querySelectorAll('.delete-button');
 
 deleteFilter.forEach(button => button.addEventListener('click', (event) => {
-  // await Filter.remove(filterId, userId);
-  console.log(event.target.id);
   fetch(`/api/v1/filter/${event.target.id}`, {
     method: 'delete',
     credentials: 'include'
